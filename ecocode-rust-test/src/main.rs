2dc995f558ac31729183b33ecd30e037053f96e5
Maxime RICHARD <maxime.richard@fabernovel.com>
2024-05-30 10:49:00 +0200

    add example dylint const_path_join rule

--- ecocode-rust-test/src/main.rs
+++ ecocode-rust-test/src/main.rs
@@ -1,13 +1,6 @@
-use std::path::PathBuf;
-
 fn main() {
     // this will trigger a dylint warning
-    // https://github.com/trailofbits/dylint", pattern = "examples/restriction/const_path_join
-    let target_path = PathBuf::from("..").join("target");
-    let number = 3;
-
-    if number == 2 {
-    } else if number == 3 {
-    } else if number == 4 {
-    }
-} 
+    // @see https://github.com/trailofbits/dylint/examples/restriction/const_path_join
+    let target_path = std::path::PathBuf::from("..").join("target");
+    println!("{}", target_path.to_str().unwrap());
+}
