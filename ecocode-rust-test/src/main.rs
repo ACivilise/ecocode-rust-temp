0cd323596c1e95f492a4fd8e4bec4a869631ae47
Maxime RICHARD <maxime.richard@fabernovel.com>
2024-05-30 11:13:08 +0200

    Feature/add dylint example rule (#1)
    * remove .gitignore slashes
    
    * add example dylint const_path_join rule

--- ecocode-rust-test/src/main.rs
+++ ecocode-rust-test/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    // this will trigger a dylint warning
+    // @see https://github.com/trailofbits/dylint/examples/restriction/const_path_join
+    let target_path = std::path::PathBuf::from("..").join("target");
+    println!("{}", target_path.to_str().unwrap());
 }
